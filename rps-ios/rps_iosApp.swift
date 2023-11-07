1876a13148def1280dc42252f59ada0775130431
serikaron <y6M-L5P-E4Y-DMF>
2023-11-07 13:24:24 +0800

    create project

---
+++ rps-ios/rps_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  rps_iosApp.swift
+//  rps-ios
+//
+//  Created by serika on 2023/11/7.
+//
+
+import SwiftUI
+
+@main
+struct rps_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
